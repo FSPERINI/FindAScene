f91c7d50dc858baec39e3b65875aecdab38983c2
Fabio <mon@mail.com>
2020-04-06 10:22:11 +0200

    ?

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -60,7 +60,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route ("/admin/{id}", name="admindelete", methods="delete")
+     * @Route ("/admin/{id}", name="admin_delete", methods="delete")
      *  @return \Symfony\Component\HttpFoundation\Response
      */
     public function delete(Salles $salles, Request $request)
