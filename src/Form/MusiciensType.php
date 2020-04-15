2012e36ae1290312defc060c39ab8952c6a011e0
Tabarnak <Maxime.wizla@hotmail.fr>
2020-04-15 18:32:26 +0200

    mise en clareté des label des form

--- src/Form/MusiciensType.php
+++ src/Form/MusiciensType.php
@@ -16,17 +16,17 @@ class MusiciensType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', null, ['label' => "Nom d'utilisateur"], ['attr' => ['class' => 'username']])
             ->add('password', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options' => array('label' => 'Mot De passe'),
+                'first_options' => array('label' => 'Mot De passe',),
                 'second_options' => array ('label' => 'Confirmation du mot de passe'),
             ))
-            ->add('mail', EmailType::class)
-            ->add('groupe')
-            ->add('city')
-            ->add('description', TextareaType::class)
-            ->add('picture')
+            ->add('mail', EmailType::class,  ['label' => "E-mail"], ['attr' => ['class' => 'email']])
+            ->add('groupe', null, ['label' => "Nom du groupe"], ['attr' => ['class' => 'username']])
+            ->add('city',  null, ['label' => "Ville"], ['attr' => ['class' => 'ville']])
+            ->add('description', TextareaType::class, ['label' => "Description"], ['attr' => ['class' => 'description_groupe']])
+            ->add('picture', null, ['label' => "Illustration"], ['attr' => ['class' => 'illustration']])
             ;           
     }
 
--- src/Form/SallesType.php
+++ src/Form/SallesType.php
@@ -16,15 +16,15 @@ class SallesType extends AbstractType
         $builder
             
             ->add('nom_salle', null, ['label' => "Nom de la salle"], ['attr' => ['class' => 'label_salle']])
-            ->add('nom_ref')
-            ->add('ville')
-            ->add('adresse')
-            ->add('mail', EmailType::class)
-            ->add('tel', TelType::class)
-            ->add('capacite')
-            ->add('categorie')
-            ->add('backline')
-            ->add('description')
+            ->add('nom_ref', null, ['label' => "Nom du référent"], ['attr' => ['class' => 'referent']])
+            ->add('ville', null, ['label' => "Ville"], ['attr' => ['class' => 'ville']])
+            ->add('adresse', null, ['label' => "Adresse"], ['attr' => ['class' => 'label_salle']])
+            ->add('mail', EmailType::class, ['label' => "E-mail"], ['attr' => ['class' => 'email']])
+            ->add('tel', TelType::class, ['label' => "N° de téléphone"], ['attr' => ['class' => 'telephone']])
+            ->add('capacite', null, ['label' => "Capacité d'accueil"], ['attr' => ['class' => 'capacite']])
+            ->add('categorie', null, ['label' => "Catégorie"], ['attr' => ['class' => 'categorie']])
+            ->add('backline', null, ['label' => "Backline"], ['attr' => ['class' => 'backline']])
+            ->add('description', null, ['label' => "Description des lieux"], ['attr' => ['class' => 'description_lieu']])
         ;
     }
 
